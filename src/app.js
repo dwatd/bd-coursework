68996b829b1b097e44248a0ca21bd74ec307c774
Kateryna Semeniuk <semenyukkate4@gmail.com>
2025-04-06 18:20:58 +0300

    changing port

--- src/app.js
+++ src/app.js
@@ -2,4 +2,19 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const PORT = 300
+const PORT = 3000
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../views");
+
+app.set('view engine', 'ejs');
+app.set('views', viewsPath);
+
+
+app.get('', (req, res) => {
+    res.send("Main page");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
